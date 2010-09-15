d2f295a8da04030fc176f3f1fa4503f0604fcc5c
Nick Black <dank@qemfd.net>
2010-09-15 07:54:29 -0400

    crib austern's TrivialContainer interface from 'generic programming and the stl' (1998)

--- src/casagrande.h
+++ src/casagrande.h
@@ -1,6 +1,31 @@
 #ifndef CASAGRANDE_CASAGRANDE
 #define CASAGRANDE_CASAGRANDE
 
-// Our container FIXME
+#include <cstddef>
+
+// Adapted from Austern 98's "Trivial Container" (p 71)
+template <class T>
+struct casagrande {
+	typedef T vtype;
+
+	typedef vtype *vptr;
+	typedef const vtype *const_vptr;
+	typedef vtype &vref;
+	typedef const vtype &const_vref;
+
+	typedef vtype *iterator;
+	typedef const vtype *const_iterator;
+
+	typedef ptrdiff_t difftype;
+	typedef size_t sizetype;
+
+	const_iterator begin() const { return 0; }
+	const_iterator end() const { return 0; }
+	sizetype size() const { return 0; }
+	bool empty() const { return true; }
+	sizetype max_size() const { return 0; }
+
+	void swap(casagrande &){}
+};
 
 #endif
