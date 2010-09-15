6f6708437a1853516af4689649bfe079c1172117
Nick Black <dank@qemfd.net>
2010-09-15 07:19:45 -0400

    set up testing skeleton

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,41 @@
+#include <iostream>
 #include <stdlib.h>
 #include <casagrande.h>
 
+// Container -- problem 1
+static int
+p1(){
+	return -1;
+}
+
+// Queue -- problem 2
+static int
+p2(){
+	return -1;
+}
+
+// Stack -- problem 3
+static int
+p3(){
+	return -1;
+}
+
 int main(void){
+	std::cout << "Performing deque tests (problem 1)..." << std::endl;
+	if(p1()){
+		std::cerr << "Error during deque tests!" << std::endl;
+		return EXIT_FAILURE;
+	}
+	std::cout << "Performing queue tests (problem 2)..." << std::endl;
+	if(p2()){
+		std::cerr << "Error during queue tests!" << std::endl;
+		return EXIT_FAILURE;
+	}
+	std::cout << "Performing stack tests (problem 3)..." << std::endl;
+	if(p3()){
+		std::cerr << "Error during stack tests!" << std::endl;
+		return EXIT_FAILURE;
+	}
+	std::cout << "All tests succeeded!" << std::endl;
 	return EXIT_SUCCESS;
 }
