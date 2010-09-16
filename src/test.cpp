aa3292d034ec967d31f98b144ff20fc9ff61879e
Nick Black <dank@qemfd.net>
2010-09-15 21:14:44 -0400

    template out p1() into fully generic tester()

--- src/test.cpp
+++ src/test.cpp
@@ -4,42 +4,34 @@
 #include <casagrande.h>
 #include <initializer_list>
 
-// Container -- problem 1
-template <class T> static int
-p1(const std::initializer_list<T> il){
+template <class C,class I,class T> static int
+tester(const std::initializer_list<T> il){
 	unsigned count;
 
-	casagrande<T> cg;
-	typename casagrande<T>::const_iterator i = cg.begin();
+	C cg;
+	I i = cg.begin();
 	cg.insert(cg.begin(), il);
 	for(count = 0 ; i != cg.end() ; ++i){
 		std::cout << "Element #" << count << ": " << *i << std::endl;
 		++count;
 	}
 	std::cout << "Iterated over " << count << " elements." << std::endl;
+	if(count != il.size()){
+		std::cerr << "Expected " << il.size() << " elements." << std::endl;
+		return -1;
+	}
 	return 0;
-}
-
-// Queue -- problem 2
-static int
-p2(){
-	return -1;
-}
-
-// Stack -- problem 3
-static int
-p3(){
-	return -1;
 }
 
 int main(void){
 	const std::initializer_list<int> il = {0, 1, 2, 3};
 
 	std::cout << "Performing deque tests (problem 1)..." << std::endl;
-	if(p1<int>(il)){
+	if(tester<casagrande<int>,casagrande<int>::const_iterator,int>(il)){
 		std::cerr << "Error during deque tests!" << std::endl;
 		return EXIT_FAILURE;
 	}
+	/*
 	std::cout << "Performing queue tests (problem 2)..." << std::endl;
 	if(p2()){
 		std::cerr << "Error during queue tests!" << std::endl;
@@ -50,6 +42,7 @@ int main(void){
 		std::cerr << "Error during stack tests!" << std::endl;
 		return EXIT_FAILURE;
 	}
+	*/
 	std::cout << "All tests succeeded!" << std::endl;
 	return EXIT_SUCCESS;
 }
